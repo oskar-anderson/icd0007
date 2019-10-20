30a85b96ac23f4381c4ff44d9927ec21be190b41
kaande <kaande@ttu.ee>
2019-10-20 20:11:09 +0300

    Table input working

--- index.php
+++ index.php
@@ -2,20 +2,68 @@
 
 require_once("vendor/tpl.php");
 
-$cmd = "show_list_page";
+
+
+$cmd = "index.php?cmd=show_list_page";
 if (isset($_GET["cmd"])) {
     $cmd = $_GET["cmd"];
 }
 
-$data = [
-    'title' => 'HW_03',
-    "footer" => "ICD0007 Homework_03"
-];
+$data = array(
+    'title' => 'HW_04',
+    "footer" => "ICD0007 Homework_04",
+);
+
+function showAddPage($data) {
+    print renderTemplate('add.html', $data);
+}
+
+function addData($data) {
+    $firstName = urlencode($_POST["firstName"]);
+    $lastName = urlencode($_POST["lastName"]);
+    $phone = urlencode($_POST["phone"]);
+
+    $file = fopen("data.txt", "a");
+    fwrite($file, $firstName);
+    fwrite($file, "\n");
+    fwrite($file, $lastName);
+    fwrite($file, "\n");
+    fwrite($file, $phone);
+    fwrite($file, "\n");
+    fclose($file);
+    showListPage($data);
+}
+
+function showListPage($data) {
+    $file = fopen("data.txt", "r");
+    if ($file) {
+        $counter = 1;
+        while (!feof($file)) {
+
+            $eesnimi = fgets($file);
+            $perekonnanimi = fgets($file);
+            $telefon = fgets($file);
+
+            $key = "eesNimi" . strval($counter);
+            $data += array("$key" => urldecode($eesnimi));
+            $key = "perekonnaNimi" . strval($counter);
+            $data += array("$key" => urldecode($perekonnanimi));
+            $key = "telefon" . strval($counter);
+            $data += array("$key" => urldecode($telefon));
+            $counter++;
+
+        }
+    }
+    fclose($file);
+
+    print renderTemplate("list.html", $data);
+}
 
 if ($cmd === "show_add_page") {
-    print renderTemplate('add.html', $data);
-
+    showAddPage($data);
+} elseif ($cmd === "add") {
+    addData($data);
 } else {
-    print renderTemplate("list.html", $data);
+    showListPage($data);
 }
 
